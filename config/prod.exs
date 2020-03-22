01725e61c23982106fdae90aee842debf321ff30
afope <afopefoluwa@unchain.io>
2020-03-22 19:44:57 +0100

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,21 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
-  url: [host: "/", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+       load_from_system_env: true,
+       url: [scheme: "https", host: "fierce-inlet-94141.herokuapp.com", port: 443],
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+       cache_static_manifest: "priv/static/cache_manifest.json",
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :blog, Blog.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: true,
+       url: System.get_env("DATABASE_URL")
 
 # ## SSL Support
 #
