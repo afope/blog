9d0954147ccdb0a11a3ed9626f4eafeaa4319217
afope <afopefoluwa@unchain.io>
2020-03-27 15:04:10 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,7 +23,7 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
-config :hello, bLOGWeb.Endpoint,
+config :hello, BlogWeb.Endpoint,
        http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
        secret_key_base: secret_key_base
 
