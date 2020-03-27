f0f88d4715f094252dec744e65784f88099a5971
afope <afopefoluwa@unchain.io>
2020-03-27 14:26:51 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,10 +11,10 @@ database_url =
     For example: ecto://USER:PASS@HOST/DATABASE
     """
 
-config :blog, Blog.Repo,
-  ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+config :hello, Blog.Repo,
+       ssl: true,
+       url: database_url,
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -23,16 +23,16 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
-config :blog, BlogWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base
+config :hello, bLOGWeb.Endpoint,
+       http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+       secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :blog, BlogWeb.Endpoint, server: true
+#     config :hello, HelloWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
